5e71eea3a7fff7aef6f21b3600536fb26e04f29e
redcozmo <redcozmo@hotmail.com>
2024-06-10 16:43:03 +0200

    add updating of valueboxes with genus and species selection

--- partials/server_valueboxes.R
+++ partials/server_valueboxes.R
@@ -1,41 +1,49 @@
-# Update of species number "values$trees_nb" for town selection
+# Update of species number "values$trees_nb" with selection
 observe({
   print("In observe values$trees_nb")
   values$trees_nb <- trees_sf %>%
     st_drop_geometry() %>%
     filter(town %in% input$which_town) %>%
+    filter(genus %in% input$which_genus) %>%
+    filter(species %in% input$which_species) %>%
     count() %>%
     as.numeric()
 })
 
-# Update of genus number "values$genus_sel_nb" for town selection
+# Update of genus number "values$genus_sel_nb" with selection
 observe({
   print("In observe values$genus_sel_nb")
   values$genus_sel_nb <- trees_sf %>%
     st_drop_geometry() %>%
     filter(town %in% input$which_town) %>%
+    filter(genus %in% input$which_genus) %>%
+    filter(species %in% input$which_species) %>%
     select(genus) %>%
     unique() %>%
     count() %>%
     as.numeric()
 })
 
-# Update of genus number "values$valueBox_tallest" for town selection
+# Update of genus number "values$valueBox_tallest" with selection
 observe({
   print("In observe values$valueBox_tallest")
   values$tallest_tree <- trees_sf %>%
     st_drop_geometry() %>%
     filter(town %in% input$which_town) %>%
+    filter(genus %in% input$which_genus) %>%
+    filter(species %in% input$which_species) %>%
     select(height_m) %>%
     max()
 })
 
-# Update of genus number "values$valueBox_largest" for town selection
+# Update of genus number "values$valueBox_largest" with selection
 observe({
   print("In observe values$valueBox_largest")
   values$largest_tree <- trees_sf %>%
     st_drop_geometry() %>%
     filter(town %in% input$which_town) %>%
+    filter(genus %in% input$which_genus) %>%
+    filter(species %in% input$which_species) %>%
     select(diameter_m) %>%
     max()
 })
